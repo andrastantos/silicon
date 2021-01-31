06e8d1410d2c318f7c9da14a29773cdae27a57b5
Andras Tantos <andras@tantosonline.com>
2021-01-30 16:55:32 -0800

    from_number for Number now checks for parameter type.

--- silicon/number.py
+++ silicon/number.py
@@ -672,6 +672,8 @@ class Number(NetType):
         return self
     @behavior
     def from_number(self, input: Junction) -> None:
+        if not isinstance(input.get_net_type(), Number):
+            raise SyntaxErrorException(f"from_number can only be used to convert from Numbers. For generic type conversion, use 'convert_from'")
         self <<= input.to_number()
 
     @classmethod
