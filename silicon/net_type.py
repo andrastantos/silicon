6d8caa2d5079471e61dc1dcd951884a12ef64f5e
Andras Tantos <andras@tantosonline.com>
2021-02-13 09:03:11 -0800

    Added documentation to NetType

--- silicon/net_type.py
+++ silicon/net_type.py
@@ -11,13 +11,29 @@ class KeyKind(PyEnum):
     Member = 1
 
 class NetType(object):
+    class Behaviors(object):
+        """
+        An empty Behaviors class to make sure the inheritance chain for behaviors has always somewhere to terminate
+        """
+        pass
+
     def __init__(self):
         self.parent_junction = None
         pass
 
     def generate_type_ref(self, back_end: 'BackEnd') -> str:
+        """
+        Generate and return a type reference that is appropriate for the given back-end.
+
+        An example would be 'logic' for a single-bit Number in Verilog
+        """
         raise NotImplementedError
     def generate_net_type_ref(self, for_junction: 'Junction', back_end: 'BackEnd') -> str:
+        """
+        Generate and return a type reference that is suitable to declare a net of the type for the given back-end.
+
+        An example would be 'input logic [7:0]' for a byte-wider input.
+        """
         raise NotImplementedError
     def is_abstract(self) -> bool:
         """
@@ -39,22 +55,78 @@ class NetType(object):
         return ""
 
     def get_unconnected_value(self, back_end: 'BackEnd') -> str:
+        """
+        Get the value used to denote unconnected values for the given back-end.
+
+        An example would be 3'x for a 3-bit value.
+
+        The default implementation, which creates a single x-value with the number
+        of bits needed to represent the values of the type is probably sufficient
+        for most derived types.
+
+        The difference between get_default_value and get_unconnected_value is subtle
+        but important: most unconnected XNets get assigned the unconnected value.
+        However an optional auto-input, if left unconnected uses the default value.
+        """
         assert back_end.language == "SystemVerilog"
         return f"{self.get_num_bits()}'x"
     def get_default_value(self, back_end: 'BackEnd') -> str:
+        """
+        Get the default used to denote unconnected values for the given back-end.
+
+        An example would be 3'0 for a 3-bit value.
+
+        The default implementation, which creates a 0 value with the number
+        of bits needed to represent the values of the type is probably sufficient
+        for most derived types.
+
+        The difference between get_default_value and get_unconnected_value is subtle
+        but important: most unconnected XNets get assigned the unconnected value.
+        However an optional auto-input, if left unconnected uses the default value.
+        """
         assert back_end.language == "SystemVerilog"
         return f"{self.get_num_bits()}'0"
     def generate_assign(self, sink_name: str, source_expression: str, xnet: 'XNet', back_end: 'BackEnd') -> str:
+        """
+        Generate and return an assignment appropriate for the given back-end
+
+        Parameters:
+        sink_name: name of the sink to which the expression to be assigned to
+        source_expression: string representation of the source to assign to the sink
+        xnet: the XNet for which the assignment is generated
+        back_end: described the back-end for which the assignment is generated.
+        """
         return f"assign {sink_name} = {source_expression};"
     def get_unconnected_sim_value(self) -> Any:
+        """
+        Get the value used to denote unconnected values for simulation purposes.
+
+        The default implementation, which returns None is probably sufficient
+        for most derived types.
+
+        The difference between get_default_sim_value and get_unconnected_sim_value
+        is subtle but important: most unconnected XNets get assigned the unconnected
+        value. However an optional auto-input, if left unconnected uses the default
+        value.
+        """
         raise None
     def get_default_sim_value(self) -> Any:
+        """
+        Get the default used to denote unconnected values for simulation purposes.
+
+        An example would be 0 for a numerical type.
+
+        The difference between get_default_sim_value and get_unconnected_sim_value
+        is subtle but important: most unconnected XNets get assigned the unconnected
+        value. However an optional auto-input, if left unconnected uses the default
+        value.
+        """
         raise NotImplementedError
     def validate_sim_value(self, sim_value: Any, parent_junction: 'Junction') -> Any:
         """
         Validates the new sim value before assignment.
 
-        Raises exceptions with approproiate error messages in case of a validation error.
+        Raises exceptions with appropriate error messages in case of a validation error.
         
         Has the option to change/correct the sim_value prior to assignment.
         
@@ -65,12 +137,18 @@ class NetType(object):
         raise NotImplementedError
     def adapt_from(self, input: 'Junction', implicit: bool) -> Optional['Junction']:
         """
-        Return None if adaptation is not supported
+        Return the (output of) a converter object that adapts the input to the current type.
+        Return None if adaptation is not supported.
+        Should support trivial conversion where input is of the same type as the current type.
+        In these cases, should simply return input.
         """
         return None
     def adapt_to(self, output_type: 'NetType', input: 'Junction', implicit: bool) -> Optional['Junction']:
         """
-        Return None if adaptation is not supported
+        Return the (output of) a converter object that adapts the current type to the desired output type.
+        Return None if adaptation is not supported.
+        Should support trivial conversion where output_type is of the same type as the current type.
+        In these cases, should simply return input.
         """
         return None
     @property
@@ -123,6 +201,18 @@ class NetType(object):
 
     @classmethod
     def create_member_setter(cls) -> 'Module':
+        """
+        Create and return a Module that's capable of collecting individual member-assignments to a type.
+
+        Used in (ex.) the following instances:
+
+        a[3] = 1
+        a[2:0] = b
+
+        where the generated RTL should become:
+
+        a = {1, b}
+        """
         raise NotImplementedError
 
     def setup_junction(self, junction: 'Junction') -> None:
@@ -161,8 +251,3 @@ class NetType(object):
         # One usually overrides only __eq__, so provide a default, compatible __ne__ implementation
         return not self == other
 
-    class Behaviors(object):
-        """
-        An empty Behaviors class to make sure the inheritance chain for behaviors has always somewhere to terminate
-        """
-        pass
