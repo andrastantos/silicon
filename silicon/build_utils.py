2977320ca880eb70797b37c4abb1b88698b42a1a
Andras Tantos <andras@tantosonline.com>
2022-05-29 18:46:45 -0700

    Lots of silly errors in build utils port from unit_tests

--- silicon/build_utils.py
+++ silicon/build_utils.py
@@ -2,12 +2,21 @@ from .exceptions import IVerilogException
 from .back_end import SystemVerilog, File
 from .module import Module, elaborate
 from .utils import BoolMarker
-from typing import Callable, Union
+from typing import Callable, Union, IO
+import os
 
 class Build:
-    __file_list = []
+    _file_list = []
 
     __skip_iverilog = BoolMarker()
+
+    class RegisteredFile(File):
+        """
+        Register the file in a global list
+        """
+        def __enter__(self) -> IO:
+            Build._file_list.append(self)
+            return super().__enter__()
 
     @staticmethod
     def generate_rtl(top_class: Union[Callable, Module], *, add_unnamed_scopes: bool = False):
@@ -17,7 +26,7 @@ class Build:
         else:
             top = top_class()
         netlist = elaborate(top, add_unnamed_scopes=add_unnamed_scopes)
-        system_verilog = SystemVerilog(stream_class = File)
+        system_verilog = SystemVerilog(stream_class = Build.RegisteredFile)
         netlist.generate(netlist, system_verilog)
         if not Build.__skip_iverilog:
             from shutil import which
@@ -25,7 +34,7 @@ class Build:
             if iverilog_path is not None:
                 from subprocess import run
                 top = netlist.get_top_level_name()
-                cmd = (iverilog_path, "-g2005-sv", f"-s{top}") + tuple(f.filename for f in test.__file_list)
+                cmd = (iverilog_path, "-g2005-sv", f"-s{top}") + tuple(f.filename for f in Build._file_list)
                 result = run(cmd)
                 if result.returncode != 0:
                     raise IVerilogException(f"IVerilog failed with error code {result.returncode}")
@@ -41,7 +50,7 @@ class Build:
 
     @staticmethod
     def clear():
-        Build.__file_list.clear()
+        Build._file_list.clear()
 
 
 # A simple decorator to skip iVerilog on tests that are known to fail on it due to iVerilog limitations
