6dd9fc99506320932205842951a1ebf55332c83f
Andras Tantos <andras@tantosonline.com>
2022-09-30 17:20:33 -0700

    New symbol table tests added.

---
+++ unit_tests/reference/test_composite_component_collision3/test_symbols.sv
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////
+// top
+////////////////////////////////////////////////////////////////////////////////
+module top (
+	input logic [7:0] in_1_r,
+	input logic [7:0] in_1_g,
+	input logic [7:0] in_1_b,
+
+	output logic [7:0] out_1_r,
+	output logic [7:0] out_1_g,
+	output logic [7:0] out_1_b
+);
+
+	logic [7:0] in_1_1_r;
+	logic [7:0] in_1_r_1;
+	logic [7:0] in_1_r_2;
+	logic [7:0] in_1_1_g;
+	logic [7:0] in_1_g_1;
+	logic [7:0] in_1_1_b;
+	logic [7:0] in_1_b_1;
+
+	assign out_1_r = in_1_r;
+	assign in_1_1_r = in_1_r;
+	assign in_1_r_1 = in_1_r;
+	assign in_1_r_2 = in_1_r;
+	assign out_1_g = in_1_g;
+	assign in_1_1_g = in_1_g;
+	assign in_1_g_1 = in_1_g;
+	assign out_1_b = in_1_b;
+	assign in_1_1_b = in_1_b;
+	assign in_1_b_1 = in_1_b;
+endmodule
+
+
--- unit_tests/test_symbols.py
+++ unit_tests/test_symbols.py
@@ -92,9 +92,119 @@ def test_wire_sub_module_name_collision():
 
     test.rtl_generation(top, inspect.currentframe().f_code.co_name)
 
+def test_local_global_collision():
+    class top(Module):
+        in_1 = Input(logic)
+        out_1 = Output(logic)
+
+        def body(self):
+            out_1 = Wire(logic)
+            self.out_1 <<= out_1
+            out_1 <<= self.in_1
+
+
+    test.rtl_generation(top, inspect.currentframe().f_code.co_name)
+
+class GPixel(NetTypeFactory, net_type = Struct):
+    @classmethod
+    def construct(cls, net_type: Optional[Struct], length: int, prefix: str="") -> Optional[Tuple[str, int, str]]:
+        if net_type is not None:
+            net_type.length = length # We'll need this later in AlphaBlender
+            net_type.add_member(prefix+"r", Unsigned(length))
+            net_type.add_member(prefix+"g", Unsigned(length))
+            net_type.add_member(prefix+"b", Unsigned(length))
+        return (f"Pixel_{length}_{prefix}", f"{length},{prefix}")
+
+def test_composite_component_collision():
+    class top(Module):
+        in_1 = Input(GPixel(8))
+        out_1 = Output(GPixel(8))
+        out_1_r = Output(logic)
+        def body(self):
+            self.out_1 <<= self.in_1
+
+    with ExpectError(SyntaxErrorException):
+        test.rtl_generation(top, inspect.currentframe().f_code.co_name)
+
+def test_composite_component_collision2():
+    class top(Module):
+        in_1 = Input(GPixel(8))
+        in_1_r = Output(logic)
+        out_1 = Output(GPixel(8))
+        def body(self):
+            self.out_1 <<= self.in_1
+
+    with ExpectError(SyntaxErrorException):
+        test.rtl_generation(top, inspect.currentframe().f_code.co_name)
+
+def test_composite_component_collision3():
+    class top(Module):
+        in_1 = Input(GPixel(8))
+        out_1 = Output(GPixel(8))
+        def body(self):
+            in_1_r = Wire(Unsigned(8))
+            in_1 = Wire(GPixel(8))
+            in_1 <<= self.in_1
+            self.out_1 <<= in_1
+            in_1_r <<= self.in_1.r
+
+
+    test.rtl_generation(top, inspect.currentframe().f_code.co_name)
+
+def test_composite_component_collision4():
+    class top(Module):
+        input1_1 = Input(GPixel(8, prefix=""))
+        input1 = Input(GPixel(8, prefix="1_"))
+        out_1 = Output(GPixel(8))
+        def body(self):
+            pass
+
+    with ExpectError(SyntaxErrorException):
+        test.rtl_generation(top, inspect.currentframe().f_code.co_name)
+
+@pytest.mark.skip(reason="This test is broken at the moment: it generates a bunch of unnamed wires, instead of named ones. Will have to re-enable once refactoring of Composites is complete")
+def test_composite_component_collision5():
+    class top(Module):
+        in_1 = Input(GPixel(8))
+        out_1 = Output(GPixel(8))
+        clk = ClkPort()
+        def body(self):
+            # All of these should be conflicting in the end
+            con                   = Wire(GPixel(8, "flict_ing_sym_bol_"))
+            con_flict             = Wire(GPixel(8, "ing_sym_bol_"))
+            con_flict_ing         = Wire(GPixel(8, "sym_bol_"))
+            con_flict_ing_sym     = Wire(GPixel(8, "bol_"))
+            con_flict_ing_sym_bol = Wire(GPixel(8))
+            con.flict_ing_sym_bol_r <<= Reg(self.in_1.r)
+            con_flict.ing_sym_bol_r <<= Reg(con.flict_ing_sym_bol_r)
+            con_flict_ing.sym_bol_r <<= Reg(con_flict.ing_sym_bol_r)
+            con_flict_ing_sym.bol_r <<= Reg(con_flict_ing.sym_bol_r)
+            con_flict_ing_sym_bol.r <<= Reg(con_flict_ing_sym.bol_r)
+
+            con.flict_ing_sym_bol_g <<= Reg(self.in_1.g)
+            con_flict.ing_sym_bol_g <<= Reg(con.flict_ing_sym_bol_g)
+            con_flict_ing.sym_bol_g <<= Reg(con_flict.ing_sym_bol_g)
+            con_flict_ing_sym.bol_g <<= Reg(con_flict_ing.sym_bol_g)
+            con_flict_ing_sym_bol.g <<= Reg(con_flict_ing_sym.bol_g)
+
+            con.flict_ing_sym_bol_b <<= Reg(self.in_1.b)
+            con_flict.ing_sym_bol_b <<= Reg(con.flict_ing_sym_bol_b)
+            con_flict_ing.sym_bol_b <<= Reg(con_flict.ing_sym_bol_b)
+            con_flict_ing_sym.bol_b <<= Reg(con_flict_ing.sym_bol_b)
+            con_flict_ing_sym_bol.b <<= Reg(con_flict_ing_sym.bol_b)
+
+            self.out_1 <<= con_flict_ing_sym_bol
+
+    test.rtl_generation(top, inspect.currentframe().f_code.co_name)
+
 
 if __name__ == "__main__":
-    test_named_sub_modules()
+    #test_named_sub_modules()
     #test_sub_module_name_collision()
     #test_wire_sub_module_name_collision()
-
+    #test_local_global_collision()
+    #test_composite_component_collision()
+    #test_composite_component_collision2()
+    #test_composite_component_collision3()
+    #test_composite_component_collision4()
+    test_composite_component_collision5()
