7f7e0be0bbb45aa56907fdd18462d2568f2dde9e
Andras Tantos <andras@tantosonline.com>
2023-03-20 18:07:59 -0700

    A better ordered set.

--- silicon/ordered_set.py
+++ silicon/ordered_set.py
@@ -1,77 +1,38 @@
-# Copyright (C) 2009 Raymond Hettinger
-
-#                          *** MIT License ***
-# Permission is hereby granted, free of charge, to any person obtaining a copy of
-# this software and associated documentation files (the "Software"), to deal in
-# the Software without restriction, including without limitation the rights to
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-# of the Software, and to permit persons to whom the Software is furnished to do
-# so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-## {{{ http://code.activestate.com/recipes/576694/ (r7)
 import collections
 
-KEY, PREV, NEXT = range(3)
-
 class OrderedSet(collections.abc.MutableSet):
-
     def __init__(self, iterable=None):
-        self.end = end = [] 
-        end += [None, end, end]         # sentinel node for doubly linked list
-        self.map = {}                   # key --> [key, prev, next]
-        if iterable is not None:
-            self |= iterable
+        if iterable is None:
+            self.map = dict()
+        else:
+            self.map = dict((item, None) for item in iterable)
 
     def __len__(self):
         return len(self.map)
 
     def __contains__(self, key):
-        return key in self.map
+        return key in self.map.keys()
 
     def add(self, key):
-        if key not in self.map:
-            end = self.end
-            curr = end[PREV]
-            curr[NEXT] = end[PREV] = self.map[key] = [key, curr, end]
+        self.map[key] = None
+
+    def remove(self, key):
+        del self.map[key]
 
     def discard(self, key):
-        if key in self.map:
-            key, prev, next = self.map.pop(key)
-            prev[NEXT] = next
-            next[PREV] = prev
+        try:
+            del self.map[key]
+        except KeyError:
+            pass
+
+    def __contains__(self, item):
+        return item in self.map.keys()
 
     def __iter__(self):
-        end = self.end
-        curr = end[NEXT]
-        while curr is not end:
-            yield curr[KEY]
-            curr = curr[NEXT]
+        return self.map.keys().__iter__()
 
     def __reversed__(self):
-        end = self.end
-        curr = end[PREV]
-        while curr is not end:
-            yield curr[KEY]
-            curr = curr[PREV]
-
-    def pop(self, last=True):
-        # changed default to last=False - by default, treat as queue.
-        if not self:
-            raise KeyError('set is empty')
-        key = next(reversed(self)) if last else next(iter(self))
-        self.discard(key)
-        return key
+        return self.map.keys().__reversed__()
 
     def __repr__(self):
         if not self:
@@ -83,15 +44,30 @@ class OrderedSet(collections.abc.MutableSet):
             return len(self) == len(other) and list(self) == list(other)
         return set(self) == set(other)
 
-    def __del__(self):
-        self.clear()                    # remove circular references
-
     def clear(self):
-        self.end = end = [] 
-        end += [None, end, end]         # sentinel node for doubly linked list
         self.map.clear()
+
+    def __ior__(self, other):
+        if isinstance(other, OrderedSet):
+            self.map |= other.map
+        else:
+            for x in other:
+                self.add(x)
+        return self
+
+class A(object):
+    def __init__(self):
+        self.a = 42
+
+def t():
+    a = (A(), A(), A())
+    return OrderedSet(a)
 
 if __name__ == '__main__':
     print(OrderedSet('abracadaba'))
     print(OrderedSet('simsalabim'))
-## end of http://code.activestate.com/recipes/576694/ }}}
+    x = t()
+    for b in x:
+        print(b.a)
+
+
