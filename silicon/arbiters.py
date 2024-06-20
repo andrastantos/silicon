11fffd51721b0b2958d312c71fc3a3cf33dce15a
Andras Tantos <andras@tantosonline.com>
2024-06-19 18:55:51 -0700

    Added support for binary output to RR arbiter.

--- silicon/arbiters.py
+++ silicon/arbiters.py
@@ -1,20 +1,29 @@
-from .module import Module
+from .module import GenericModule
 from .port import Input, Output, Wire
 from .auto_input import ClkPort, RstPort
 from .exceptions import SyntaxErrorException
-from .number import logic, Unsigned
+from .number import logic, Unsigned, Number
 from .number import is_number
 from .primitives import concat, Reg, Select, SelectFirst
+from enum import Enum
 
-class RoundRobinArbiter(Module):
+
+class RoundRobinArbiter(GenericModule):
+
+    class OutputKind(Enum):
+        OneHot = 0
+        Binary = 1
+
     from itertools import chain as _chain
     """
     Simple round-robin arbiter.
 
     - Input is a bit-vector, with each bit corresponding to a requestor.
       The highest order bit has the highest priority.
-    - The output is a one-hot encoded bit-vector, where the single set bit
-      corresponds to the requestor winning the arbitration.
+    - The output is either
+        - A one-hot encoded bit-vector, where the single set bit
+          corresponds to the requestor winning the arbitration.
+        - A binary number corresponding to the selected port
     - An additional 'advance' input is used to (on the rising edge of clock)
       advance to the next round-robin scheduling decision
     - Finally a 'restart' input can be used to reset the round-robin decision
@@ -33,15 +42,21 @@ class RoundRobinArbiter(Module):
     advance = Input(logic)
     restart = Input(logic, default_value = 0)
 
-    def construct(self):
-        pass
+    def construct(self, output_kind: 'RoundRobinArbiter.OutputKind' = None):
+        if output_kind is None: output_kind = RoundRobinArbiter.OutputKind.OneHot
+        self.output_kind = output_kind
 
     def body(self) -> None:
+        requestor_cnt = self.requestors.get_num_bits()
+
         if not is_number(self.requestors.get_net_type()):
             raise SyntaxErrorException(f"RoundRobinArbiter {self} only supports Numbers as the requestor input type")
-        self.grants.set_net_type(self.requestors.get_net_type())
+        if self.output_kind == RoundRobinArbiter.OutputKind.OneHot:
+            grant_type = self.requestors.get_net_type()
+        else:
+            grant_type = Number(min_val=0, max_val=requestor_cnt-1)
 
-        requestor_cnt = self.requestors.get_num_bits()
+        self.grants.set_net_type(grant_type)
         mask = Wire(Unsigned(requestor_cnt-1))
 
         # Mask contains N number of 1's: adding a 1 every type we advance, and resetting to 0, when it's all 1's.
@@ -69,20 +84,29 @@ class RoundRobinArbiter(Module):
         # we use the second (unasked) priority encoder. This encoder will look at all inputs, but
         # (since we know that the bottom N bits are 0) in practice will only select from the top (masked) inputs.
 
+        if self.output_kind == RoundRobinArbiter.OutputKind.OneHot:
+            requestor_values = tuple(1<< i for i in range(requestor_cnt-1,-1,-1))
+        else:
+            requestor_values = tuple(i for i in range(requestor_cnt-1,-1,-1))
+
         masked_requestors = Wire(self.requestors.get_net_type())
         masked_requestors <<= self.requestors & ~concat(mask, "1'b0")
         masked_selector = SelectFirst(
-            *(RoundRobinArbiter._chain.from_iterable((req, idx) for req, idx in zip(reversed(masked_requestors), (1<< i for i in range(requestor_cnt-1,-1,-1))))),
+            *(RoundRobinArbiter._chain.from_iterable((req, idx) for req, idx in zip(reversed(masked_requestors), requestor_values))),
+            default_port = 0
+        )
+        use_masked_selector = SelectFirst(
+            *(RoundRobinArbiter._chain.from_iterable((req, idx) for req, idx in zip(reversed(masked_requestors), (1 for i in range(requestor_cnt-1,-1,-1))))),
             default_port = 0
         )
 
         unmasked_selector = SelectFirst(
-            *(RoundRobinArbiter._chain.from_iterable((req, idx) for req, idx in zip(reversed(self.requestors), (1<< i for i in range(requestor_cnt-1,-1,-1))))),
+            *(RoundRobinArbiter._chain.from_iterable((req, idx) for req, idx in zip(reversed(self.requestors), requestor_values))),
             default_port = 0
         )
 
         self.grants <<= Select(
-            masked_selector == 0,
-            masked_selector,
-            unmasked_selector
+            use_masked_selector,
+            unmasked_selector,
+            masked_selector
         )
--- unit_tests/test_arbiters.py
+++ unit_tests/test_arbiters.py
@@ -21,7 +21,8 @@ def test_round_robin_arbiter():
             self.grants = Wire(Unsigned(8))
             self.advance = Wire(logic)
 
-            self.grants <<= RoundRobinArbiter(self.requestors, self.advance)
+            dut = RoundRobinArbiter()
+            self.grants <<= dut(self.requestors, self.advance)
 
         def simulate(self, simulator: Simulator):
             seed(0)
@@ -58,6 +59,54 @@ def test_round_robin_arbiter():
 
     test.simulation(top, inspect.currentframe().f_code.co_name, add_unnamed_scopes=True)
 
+def test_round_robin_arbiter_binary():
+    class top(Module):
+        def body(self):
+            self.clk = Wire(logic)
+            self.rst = Wire(logic)
+            self.requestors = Wire(Unsigned(8))
+            self.grants = Wire(Unsigned(3))
+            self.advance = Wire(logic)
+
+            dut = RoundRobinArbiter(RoundRobinArbiter.OutputKind.Binary)
+            self.grants <<= dut(self.requestors, self.advance)
+
+        def simulate(self, simulator: Simulator):
+            seed(0)
+            def clk() -> int:
+                yield 5
+                self.clk <<= ~self.clk & self.clk
+                yield 5
+                self.clk <<= ~self.clk
+                yield 0
+
+            print("Simulation started")
+
+            self.rst <<= 1
+            self.clk <<= 1
+            self.advance <<= 0
+            yield 10
+            for i in range(5):
+                yield from clk()
+            self.rst <<= 0
+
+            self.requestors <<= 0xff
+            yield from clk()
+            simulator.sim_assert(self.grants == 7)
+            self.advance <<= 1
+            for i in range(16):
+                yield from clk()
+                simulator.sim_assert(self.grants == (7-(i % 8)))
+
+            for i in range(8):
+                self.requestors <<= 1 << i
+                for j in range(8):
+                    yield from clk()
+                    simulator.sim_assert(self.grants == i)
+
+    test.simulation(top, inspect.currentframe().f_code.co_name, add_unnamed_scopes=True)
+
 
 if __name__ == "__main__":
-    test_round_robin_arbiter()
+    #test_round_robin_arbiter()
+    test_round_robin_arbiter_binary()
